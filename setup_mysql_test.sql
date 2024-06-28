5f36dcc8500d5d570fd562e27f2b0e1e21ebfc39
Tanner Saint <tsaint2004@gmail.com>
2024-06-28 22:24:12 +0000

    fixed syntax errors

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,12 @@
 -- creates hbnb_test_db database
 -- creates hbnb_test user and grants privileges
 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
