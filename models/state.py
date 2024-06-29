2356d17de54285cd60504ef8330685739282251e
Tanner Saint <tsaint2004@gmail.com>
2024-06-29 02:48:19 +0000

    added cities relationship to state.py

--- models/state.py
+++ models/state.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+import os
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     name = Column(String(128), nullable=False)
     __tablename__ = "states"
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="delete")
 
