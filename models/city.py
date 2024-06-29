750109724f814834cf2ffb69ecaf213f821f328d
Tanner Saint <tsaint2004@gmail.com>
2024-06-29 02:25:16 +0000

    updated city.py attributes

--- models/city.py
+++ models/city.py
@@ -2,8 +2,8 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
+    name = Column(String(128), nullable=False)
+    __tablename__ = "cities"
