0ce502938a8bcad55dbe9740c6b96a923d5b8ad4
Tanner Saint <tsaint2004@gmail.com>
2024-06-30 21:46:34 +0000

    added comment

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+"""Now includes switch for new storage system"""
 import models
 from models.base_model import BaseModel
 from models.base_model import Base
