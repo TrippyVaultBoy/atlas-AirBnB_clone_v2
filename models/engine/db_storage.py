a6f1c6bea11a142a51b92a201e717975ac468022
Tanner Saint <tsaint2004@gmail.com>
2024-07-01 04:27:57 +0000

    added methods to db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,6 +12,13 @@ import models.place
 import models.amenity
 import models.review
 
+classes = {"State": models.state.State,
+           "City": models.city.City,
+           "User": models.user.User,
+           "Place": models.place.Place,
+           "Amenity": models.amenity.Amenity,
+           "Review": models.review.Review}
+
 
 class DBStorage:
     """This class manages data storage for a SQL Database"""
@@ -29,6 +36,38 @@ class DBStorage:
         if os.getenv(HBNB_ENV) == 'test':
             Base.metadata.drop_all(self.__engine)
 
-    def all(self, cls=None):)
+    def all(self, cls=None):
+        """query on the current database session (self.__session)
+        all objects depending of the class name (argument cls)"""
+        objects = {}
+        if cls is None:
+            for cls_obj in classes:
+                objs = self.__session.query(classes[cls_obj]).all()
+                for obj in objs:
+                    key = f"{obj.__class__.__name__}.{obj.id}"
+                    objects[key] = obj
+        else:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = f"{obj.__class__.__name__}.{obj.id}"
+                objects[key] = obj
+        return objects
+    
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
 
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
 
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        session_make = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session = scoped_session(session_make)
+        self.__session = session()
