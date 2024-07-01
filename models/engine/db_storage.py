bcee49db1fdd6e8d666981b704459280e0b6a912
Tanner Saint <tsaint2004@gmail.com>
2024-07-01 16:49:11 +0000

    changed grant command in setup_mysql_dev.sql

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -31,7 +31,15 @@ class DBStorage:
         HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
         HBNB_ENV = os.getenv('HBNB_ENV', 'development')
-        self.__engine = create_engine('mysql+mysqldb://hbnb_dev:hbnb_dev_pwd@localhost/hbnb_dev_db', pool_pre_ping=True)
+
+        print(HBNB_MYSQL_HOST)
+        print(HBNB_MYSQL_PWD)
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(HBNB_MYSQL_USER,
+                                             HBNB_MYSQL_PWD,
+                                             HBNB_MYSQL_HOST,
+                                             HBNB_MYSQL_DB))
 
         if os.getenv(HBNB_ENV) == 'test':
             Base.metadata.drop_all(self.__engine)
