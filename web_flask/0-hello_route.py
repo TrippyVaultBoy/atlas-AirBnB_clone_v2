02b27c0862aa6e8f952fcf46c331c463fed718b6
Tanner Saint <tsaint2004@gmail.com>
2024-07-09 21:04:26 +0000

    added documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""This module starts a web Flask application"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def Hello_HBNB():
+    """prints Hello HBNB!"""
     return "Hello HBNB!"
 
 if __name__ == '__main__':
