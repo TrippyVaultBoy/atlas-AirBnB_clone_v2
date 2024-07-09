d80f761408dc4433e34b0a1543bd8aef91960c21
Tanner Saint <tsaint2004@gmail.com>
2024-07-09 21:08:04 +0000

    added HBNB() method

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def Hello_HBNB():
     """prints Hello HBNB!"""
     return "Hello HBNB!"
+
+
+@app.route(/hbnb, strict_slashes=False)
+def HBNB():
+    """prints HBNB"""
+    return ("HBNB")
 
 
 if __name__ == '__main__':
