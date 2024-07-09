8e9730bc3c8208f27d1e7c1779dda3e13178a5e7
Tanner Saint <tsaint2004@gmail.com>
2024-07-09 21:05:14 +0000

    made pep8 compliant

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def Hello_HBNB():
     """prints Hello HBNB!"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
