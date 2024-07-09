a85742f937ac2de2e5cc28fca0cde3dbc68b354f
Tanner Saint <tsaint2004@gmail.com>
2024-07-09 21:55:05 +0000

    added 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module starts a web Flask application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello_HBNB():
+    """prints Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """prints HBNB"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """displays c"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """displays python"""
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    """displays a number"""
+    if isinstance(n, int):
+        return ("{} is a number".format(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
