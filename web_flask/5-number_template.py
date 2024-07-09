413f7f29e746d7dd4c821c7d1fdeb545b729ffd7
Tanner Saint <tsaint2004@gmail.com>
2024-07-09 22:07:46 +0000

    added template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This module starts a web Flask application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello_HBNB():
+    """prints Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """prints HBNB"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """displays c"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """displays python"""
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """displays a number"""
+    return ("{} is a number".format(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """displays html page"""
+    return (render_template("5-number.html", n=n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
